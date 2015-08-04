91c60863d4c8bf4a47d1a3568fd40d09ac716246
AndreiBarsan <andrei.ioan.barsan@gmail.com>
2015-08-04 11:49:40 +0300

    Changed default vim colorscheme. Default git credential helper in OSX.

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -24,3 +24,8 @@ git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
+
+# Remember git credentials on OSX
+if [[ 'Darwin' == $(uname) ]]; then
+  git config --global credential.helper osxkeychain
+fi
