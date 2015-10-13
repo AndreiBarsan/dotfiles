febc6dfc043a472f6500ab6dd6f7a3897765d105
AndreiBarsan <andrei.ioan.barsan@gmail.com>
2015-10-13 22:23:22 +0200

    LaTeX support in vim; some school-specific aliases to make my life easier; svn plugin in zsh (which doesn't seem to work);

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,11 @@
 alias reload!='. ~/.zshrc'
+
+master='/Users/andrei/Copy/~Master'
+
+alias al='master && cd "AL-(Algorithms-Lab)"'
+alias asl='master && cd "ASL-(Advanced-Systems-Lab)"'
+alias dm='master && cd "DM-(Data-Mining)"'
+alias ir='master && cd "IR-(Information-Retrieval)"'
+alias ml='master && cd "ML-(Machine-Learning)"'
+alias pai='master && cd "PAI-(Probabilistic-AI)"'
+alias sdb='master && cd "SDB-(Scientific-Databases)"'
