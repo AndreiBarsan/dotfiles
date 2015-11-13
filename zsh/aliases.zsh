c661cbef01dc723df0693d0fde1d55efdc65aa69
AndreiBarsan <andrei.ioan.barsan@gmail.com>
2015-11-13 14:29:13 +0100

    Minor vimrc tweaks.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,6 +2,7 @@ alias reload!='. ~/.zshrc'
 
 master='/Users/andrei/Copy/~Master'
 
+# 1st Semester
 alias al='master && cd "AL-(Algorithms-Lab)"'
 alias asl='master && cd "ASL-(Advanced-Systems-Lab)"'
 alias dm='master && cd "DM-(Data-Mining)"'
@@ -9,3 +10,8 @@ alias ir='master && cd "IR-(Information-Retrieval)"'
 alias ml='master && cd "ML-(Machine-Learning)"'
 alias pai='master && cd "PAI-(Probabilistic-AI)"'
 alias sdb='master && cd "SDB-(Scientific-Databases)"'
+
+# gcalcli (Google Calendar)
+alias agenda='gcalcli agenda'
+alias calw='gcalcli calw'
+alias calm='gcalcli calm'
