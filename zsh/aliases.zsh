027b2d796cfefbc5f5df08b66e1d432617e64fbe
AndreiBarsan <andrei.ioan.barsan@gmail.com>
2016-03-02 14:54:07 +0100

    Second semester aliases.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,8 +1,8 @@
 alias reload!='. ~/.zshrc'
 
-master='/Users/andrei/Copy/~Master'
+alias master='cd /Users/andrei/polybox/~Master'
 
-# 1st Semester
+# 1st semester
 alias al='master && cd "AL-(Algorithms-Lab)"'
 alias asl='master && cd "ASL-(Advanced-Systems-Lab)"'
 alias dm='master && cd "DM-(Data-Mining)"'
@@ -10,6 +10,14 @@ alias ir='master && cd "IR-(Information-Retrieval)"'
 alias ml='master && cd "ML-(Machine-Learning)"'
 alias pai='master && cd "PAI-(Probabilistic-AI)"'
 alias sdb='master && cd "SDB-(Scientific-Databases)"'
+
+# 2nd semester
+alias acd='master && cd ACD*'
+alias amr='master && cd AMR*'
+alias cil='master && cd CIL*'
+alias dal='master && cd DAL*'
+alias nlp='master && cd NLP*'
+alias sem='master && cd SEM*'
 
 # gcalcli (Google Calendar)
 alias agenda='gcalcli agenda'
