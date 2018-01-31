4b58b0009e53ab28473ad7f290502a4aae8457c0
AndreiBarsan <andrei.ioan.barsan@gmail.com>
2018-01-31 10:53:44 -0500

    Move generic aliases to generic directory

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -13,3 +13,29 @@ alias gib='sudo apt-get install'
 
 # Nosy neighbor! :)
 alias ww='watch -n 0.5 w'
+
+# For jumping around
+alias phd="cd $HOME/drive/PhD"
+
+# gcalcli (Google Calendar)
+alias agenda='gcalcli agenda'
+alias calw='gcalcli calw'
+alias calm='gcalcli calm'
+
+# [OSX] Force regular vim to be MacVim's vim
+if [[ "$(uname -s)" == "Darwin" ]]; then
+  alias vim='DISPLAY=:0 reattach-to-user-namespace /Applications/MacVim.app/Contents/MacOS/Vim --servername vim-server'
+  alias mvim='DISPLAY=:0 reattach-to-user-namespace mvim --servername vim-server'
+  alias open='DISPLAY=:0 reattach-to-user-namespace open'
+else
+  # Temporarily disabled since the version on U of T's servers doesn't have
+  # server support *grumble grumble*.
+  # alias vim='vim --servername vim-server'
+  alias open='xdg-open'
+fi
+
+# Traditional safety measure
+alias mv='mv -i'
+alias rm='rm -i'
+alias cp='cp -i'
+
