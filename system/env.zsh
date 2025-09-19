6fc6ab00cfcfc31de73a000f74c98ceb38ec2e66
Ioan Andrei Bârsan <andreibarsan@users.noreply.github.com>
2025-09-19 10:45:09 -0400

    Minor tmux tweaks

--- system/env.zsh
+++ system/env.zsh
@@ -10,6 +10,9 @@ else
   export EDITOR='vim'
 fi
 
+# 2024-08-25 macOS
+export PATH="$HOME/Library/Python/3.12/bin/powerline:$PATH"
+
 # Default way of installing (local user, recommended):
 #   pip install --user powerline-status
 if [[ $(uname -s) == 'Linux' ]]; then
@@ -19,5 +22,6 @@ if [[ $(uname -s) == 'Linux' ]]; then
 elif [[ $(hostname) == 'andreib-C02Y71MJJGH6' ]]; then
   export TMUX_POWERLINE_CONF="/usr/local/lib/python3.7/site-packages/powerline/bindings/tmux/powerline.conf"
 else
-  export TMUX_POWERLINE_CONF="$HOME/.local/lib/python3.7/site-packages/powerline/bindings/tmux/powerline.conf"
+  #export TMUX_POWERLINE_CONF="$HOME/.local/lib/python3.10/site-packages/powerline/bindings/tmux/powerline.conf"
+  export TMUX_POWERLINE_CONF="$HOME/Library/Python/3.12/lib/python/site-packages/powerline/bindings/tmux/powerline.conf"
 fi
